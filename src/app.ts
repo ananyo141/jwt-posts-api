9a514aa7839b4c98908e3a19369e748cbf1b1f8a
Ananyobrata Pal <ananyo141@gmail.com>
2023-04-02 10:38:35 +0530

    await mongo connection

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(errorHandler);
 const port = process.env.PORT || 8000;
 const start = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGO_URI!);
     console.log("Connected to MongoDB");
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
